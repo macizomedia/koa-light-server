114fe30038e9b7fcd0284fd684dd5e037214e442
Blas Oronoz <macizomedia@users.noreply.github.com>
2020-05-29 20:57:38 +0200

    Auth controller updated
    Tentative ways commit

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,6 +1,16 @@
-module.exports = async (ctx, next) => {
+const controller = require('../controllers/auth');
+const validate = require('../controllers/auth.validate');
+const trimRequest = require('trim-request');
+
+exports.login = async (ctx, next) => {
     ctx.status = 200
     console.log('auth middleware reached')
 	// passport 
+	trimRequest.All
+	validate.login
+	controller.login
 	await (next)
 };
+
+exports.register = async (ctx, next) => {
+	
